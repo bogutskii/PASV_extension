147e5fd9ffe4a26dbba900a44f3a89a761d63124
bogutskii <sl9site@gmail.com>
2024-01-10 22:02:35 -0800

    ver 1.3 release

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    if (key === "classModifierEnabled" && newValue === true) {
-      modifyPageClasses();
-    }
+chrome.storage.local.get(["classModifierEnabled"], function (result) {
+  if (result.classModifierEnabled) {
+    // Добавляем задержку перед выполнением функции modifyPageClasses
+    setTimeout(modifyPageClasses, 2000); // 2000 миллисекунд = 2 секунды
   }
 });
 
@@ -12,3 +11,15 @@ function modifyPageClasses() {
     el.className = el.className.replace(/col-lg-\d+/g, "col-lg-12");
   });
 }
+
+// Слушаем изменения в chrome.storage
+chrome.storage.onChanged.addListener(function (changes, area) {
+  if (area == "local" && "classModifierEnabled" in changes) {
+    if (changes.classModifierEnabled.newValue) {
+      setTimeout(modifyPageClasses, 2000); // Добавляем задержку
+    } 
+    // else {
+    //   window.location.reload(); // Перезагружаем страницу, если функционал отключен
+    // }
+  }
+});
--- popup.js
+++ popup.js
@@ -1,28 +1,59 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("toggleClassChange");
+  const updateButton = document.getElementById("updateButton");
 
-  // Загружаем текущее состояние переключателя
   chrome.storage.local.get(["classModifierEnabled"], function (result) {
-    toggle.checked = result.classModifierEnabled || false;
+    toggle.checked = result.classModifierEnabled !== false;
   });
 
   toggle.addEventListener("change", function () {
     chrome.storage.local.set({ classModifierEnabled: this.checked });
+    updateClassesIfNeeded(this.checked);
+  });
+
+  updateButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         function: modifyPageClasses,
-        args: [toggle.checked],
       });
     });
   });
 });
 
-function modifyPageClasses(enableModification) {
-  if (!enableModification) return;
-
+function modifyPageClasses() {
   const allClasses = document.querySelectorAll('*[class^="col-lg-"]');
   allClasses.forEach((el) => {
     el.className = el.className.replace(/col-lg-\d+/g, "col-lg-12");
   });
+  function removeSpecificStyleFromDeepDiv() {
+    console.log("removeSpecificStyleFromDeepDiv");
+    var divs = document.querySelectorAll("div[style]");
+
+    divs.forEach(function (div) {
+      var style = div.getAttribute("style");
+      if (style.includes("height: 1184.4px;")) {
+        var updatedStyle = style.replace("height: 1184.4px;", "").trim();
+        div.setAttribute("style", updatedStyle);
+      }
+    });
+  }
+  removeSpecificStyleFromDeepDiv();
+}
+
+function updateClassesIfNeeded(isEnabled) {
+  if (isEnabled) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: modifyPageClasses,
+      });
+    });
+  } else {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs[0]) {
+        chrome.tabs.reload(tabs[0].id);
+      }
+    });
+  }
 }
